f20ff59b4dd4f4b980f21c75ee810cc2df7f5f57
c.klewar <c.klewar@f5.com>
2024-10-14 15:16:20 +0200

    0.11.37 v4

--- versions.tf
+++ versions.tf
@@ -26,6 +26,11 @@ terraform {
       version = ">= 3.25.0"
     }
 
+    http-full = {
+      source  = "salrashid123/http-full"
+      version = ">= 1.3.1"
+    }
+
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }
